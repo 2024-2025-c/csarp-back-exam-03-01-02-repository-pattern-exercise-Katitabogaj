46f152717fdd28be6d62e7f022130af9fd97db55
Katitabogaj <c9csomar@vasvari.org>
2024-12-17 11:31:21 +0100

    konstruktorban dependency injection a Repoból

--- Kreata.Backend/Controllers/TeacherController.cs
+++ Kreata.Backend/Controllers/TeacherController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Kreata.Backend.Repos;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Kreata.Backend.Controllers
 {
@@ -6,5 +7,11 @@ namespace Kreata.Backend.Controllers
     [Route("api/[controller]")]
     public class TeacherController : ControllerBase
     {
+        private ITeacherRepo _teacherRepo;
+
+        public TeacherController(ITeacherRepo? teacherRepo)
+        {
+            _teacherRepo = teacherRepo ?? throw new ArgumentNullException(nameof(teacherRepo));
+        }
     }
 }
